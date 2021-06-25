252e32b663320823c1f9711cb6e6c98354d5f0f4
Domiciano <domiciano.rincon@correounivalle.edu.co>
2021-06-25 12:08:49 -0500

    Update HTTPSWebUtilDomi.kt

--- app/src/main/java/edu/co/icesi/semana9kotlina/util/HTTPSWebUtilDomi.kt
+++ app/src/main/java/edu/co/icesi/semana9kotlina/util/HTTPSWebUtilDomi.kt
@@ -26,6 +26,19 @@ class HTTPSWebUtilDomi {
         return client.inputStream.bufferedReader().readText()
     }
 
+    fun POSTtoFCM(json:String){
+        val url = URL("https://fcm.googleapis.com/fcm/send")
+        val client = url.openConnection() as HttpsURLConnection
+        client.requestMethod = "POST"
+        client.setRequestProperty("Authorization", "key=AAAAjo_3vA4:APA91bE-69eVH7odJplYUvZFv4mfqKGUqRXCqk8GSVc2RrqQUZ8l0sGF9t5ljeYhsV5HL3aQdVDNQlzFm3GYSVBVFeVZ4ASsWD_57dzAkT0evd3rCo1OIZqd2tRiqyaOdvoQCMZq6DUm")
+        client.setRequestProperty("Content-Type", "application/json")
+        client.doOutput = true
+        client.outputStream.bufferedWriter().use {
+            it.write(json)
+            it.flush()
+        }
+    }
+    
     fun PUTRequest(url: String, json: String): String {
         val url = URL(url)
         val client = url.openConnection() as HttpsURLConnection
